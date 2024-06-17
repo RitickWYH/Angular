7a5335e11db2edd0defd2ad25708ff4a672bffd2
WYH-IT-G1-003 <WYH-IT-G1-003@DESKTOP-US8050M>
2024-06-17 19:00:32 +0530

    FormsModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+})
+
+@NgModule({
+  imports: [FormsModule]
+
 })
 export class AppComponent {
   title = 'Profile';
